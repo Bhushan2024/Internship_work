bd41d1c48f7f7c180227d893b358bd55578f1ae8
Bhushan2024 <98589863+Bhushan2024@users.noreply.github.com>
2023-03-18 11:57:30 +0530

    Add files via upload

---
+++ Campany Page/index.js
@@ -0,0 +1,17 @@
+function ShowAndHide() {
+    var x = document.getElementById('SectionName');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function ShowAndHidee() {
+    var x = document.getElementById('SectionNamee');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
